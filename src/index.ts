3226fda4f1bb5e830e12fe5cb9ba6b6c8a4b4021
Aliaksei <sladkiy.aleksey@gmail.com>
2023-01-04 15:16:37 +0100

    feat: add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import * as dotenv from "dotenv"
-dotenv.config()
-console.log(process.env.PORT)
+import * as dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT;
+console.log(port);
