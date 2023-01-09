5fb9993782ea504af26d8b4d700a4dea1164df09
Aliaksei <sladkiy.aleksey@gmail.com>
2023-01-09 19:20:53 +0100

    feat: add parse Body (JSON) in getBody

--- src/app/helpers/getBody.ts
+++ src/app/helpers/getBody.ts
@@ -1,6 +1,8 @@
-import { IncomingMessage } from 'node:http';
+import { IncomingMessage, ServerResponse } from 'node:http';
+import { StatusCode, ErrorMessages } from '../helpers/responseMessages';
+import { IPropertiesOfBody } from './model.propertiesOfBody';
 
-export function getBody(request: IncomingMessage): Promise<string> {
+export function getBody(request: IncomingMessage, response: ServerResponse): Promise<IPropertiesOfBody> {
   return new Promise((resolve) => {
     let body = '';
 
@@ -9,7 +11,13 @@ export function getBody(request: IncomingMessage): Promise<string> {
     });
 
     request.on('end', () => {
-      resolve(body);
+      try {
+        const bodyParse = JSON.parse(body);
+        resolve(bodyParse);
+      } catch {
+        response.writeHead(StatusCode.badRequest, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({ code: StatusCode.badRequest, message: ErrorMessages.invalidJson }));
+      }
     });
   });
 }
