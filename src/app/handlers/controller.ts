c86f844de5853db790ca55b39df6f3d9d4cc857f
Aliaksei <sladkiy.aleksey@gmail.com>
2023-01-09 19:24:41 +0100

    feat: add properties in Controller

--- src/app/handlers/controller.ts
+++ src/app/handlers/controller.ts
@@ -1,33 +1,71 @@
 import { IncomingMessage, ServerResponse } from 'node:http';
-import { IUser } from '../user/user.model';
+import { StatusCode, ErrorMessages } from '../helpers/responseMessages';
+import { requiredPropertiesOfBody } from '../helpers/requiredPropertiesOfBody';
 import { getBody } from '../helpers/getBody';
-import  { user } from '../user/user';
+import { checkId } from '../helpers/checkId';
+import { user } from '../user/user';
 
 export class Controller {
-  getUser(request: IncomingMessage, response: ServerResponse, id: string) {
-    user.findById(id);
+  getUser(response: ServerResponse, id: string) {
+    try {
+      checkId(response, id);
+      const userById = user.findById(id);
+      response.writeHead(StatusCode.ok, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(userById));
+    } catch {
+      response.writeHead(StatusCode.internalServerError, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ code: StatusCode.internalServerError, message: ErrorMessages.serverError }));
+    }
   }
 
-  getAllUser(request: IncomingMessage, response: ServerResponse) {
-    const allUsers = user.findAll();
-    response.writeHead(201, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(allUsers));
+  getAllUser(response: ServerResponse) {
+    try {
+      const allUsers = user.findAll();
+      response.writeHead(StatusCode.ok, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(allUsers));
+    } catch {
+      response.writeHead(StatusCode.internalServerError, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ code: StatusCode.internalServerError, message: ErrorMessages.serverError }));
+    }
   }
 
   async createUser(request: IncomingMessage, response: ServerResponse) {
-    const body = await getBody(request);
-    const { username, age, hobbies }: Omit<IUser, 'id'> = JSON.parse(body);
-    const newUser = user.create({ username, age, hobbies });
-    response.writeHead(201, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(newUser));
+    try {
+      const body = await getBody(request, response);
+      requiredPropertiesOfBody(response, body);
+      const newUser = user.create(body);
+      response.writeHead(StatusCode.created, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(newUser));
+    } catch {
+      response.writeHead(StatusCode.internalServerError, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ code: StatusCode.internalServerError, message: ErrorMessages.serverError }));
+    }
   }
 
-  updateUser() {
-
+  async updateUser(request: IncomingMessage, response: ServerResponse, id: string) {
+    try {
+      checkId(response, id);
+      const body = await getBody(request, response);
+      requiredPropertiesOfBody(response, body);
+      const updateUser = user.update(id, body);
+      response.writeHead(StatusCode.ok, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(updateUser));
+    } catch {
+      response.writeHead(StatusCode.internalServerError, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ code: StatusCode.internalServerError, message: ErrorMessages.serverError }));
+    }
   }
 
-  deleteUser() {
-
+  deleteUser(response: ServerResponse, id: string) {
+    try {
+      checkId(response, id);
+      const deleteUser = user.delete(id);
+      response.writeHead(StatusCode.noContent, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(deleteUser));
+    } catch {
+      response.writeHead(StatusCode.internalServerError, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ code: StatusCode.internalServerError, message: ErrorMessages.serverError }));
+    }
   }
 }
 
